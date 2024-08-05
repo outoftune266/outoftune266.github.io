4c43f814409a6255680bc013c759c311848d5379
outoftune266 <zakmonnet@gmail.com>
2024-08-05 11:38:07 -0500

    updated portfolio projects

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -4,27 +4,35 @@ import Project from "../project/project";
 class Portfolio extends React.Component {
   state = {
     projects: [
+      // {
+      //   name: "K.I.T. - Keep in Touch",
+      //   description:
+      //     "A closed group messaging app with Web Socket functionality",
+      //   github: "https://github.com/Andrea-Boyd/Project-3",
+      //   href: "https://sheltered-plains-45359.herokuapp.com/",
+      //   preview: "/assets/images/login_page.PNG",
+      // },
+      // {
+      //   name: "Time Zone Traveler",
+      //   description: "Find interesting travel stops and add your own",
+      //   github: "https://github.com/outoftune266/Time-Zone-Traveler",
+      //   href: "https://eatandplay.herokuapp.com/",
+      //   preview: "/assets/images/Time Zone Traveler.png",
+      // },
+      // {
+      //   name: "Song Scout",
+      //   description: "Learn more about your favorite artists and albums",
+      //   github: "https://github.com/outoftune266/Song-Scout",
+      //   href: "https://outoftune266.github.io/Song-Scout/",
+      //   preview: "assets/images/songscout.png",
+      // },
       {
-        name: "K.I.T. - Keep in Touch",
+        name: "Card Game Counter",
         description:
-          "A closed group messaging app with Web Socket functionality",
-        github: "https://github.com/Andrea-Boyd/Project-3",
-        href: "https://sheltered-plains-45359.herokuapp.com/",
-        preview: "/assets/images/login_page.PNG",
-      },
-      {
-        name: "Time Zone Traveler",
-        description: "Find interesting travel stops and add your own",
-        github: "https://github.com/outoftune266/Time-Zone-Traveler",
-        href: "https://eatandplay.herokuapp.com/",
-        preview: "/assets/images/Time Zone Traveler.png",
-      },
-      {
-        name: "Song Scout",
-        description: "Learn more about your favorite artists and albums",
-        github: "https://github.com/outoftune266/Song-Scout",
-        href: "https://outoftune266.github.io/Song-Scout/",
-        preview: "assets/images/songscout.png",
+          "Mobile-first application to keep score in your favorite card games!",
+        github: "https://github.com/outoftune266/gamecounter",
+        href: "https://outoftune266.github.io/gamecounter/",
+        preview: "/assets/images/Card_Preview.png",
       },
       {
         name: "Weather Dashboard",
@@ -35,12 +43,20 @@ class Portfolio extends React.Component {
         preview: "/assets/images/weather.png",
       },
       {
-        name: "Eat That Burger!",
-        description: "Cloud database for tracking burgers eaten",
-        github: "https://github.com/outoftune266/13-Eat-That-Burger",
-        href: "https://evening-lowlands-70965.herokuapp.com/",
-        preview: "/assets/images/eatthatburger.png",
+        name: "Work Day Scheduler",
+        description:
+          "Add tasks to your 9-5 workday",
+        github: "https://github.com/outoftune266/05-Work-Day-Scheduler/",
+        href: "https://outoftune266.github.io/05-Work-Day-Scheduler/",
+        preview: "/assets/images/work_day.png",
       },
+      // {
+      //   name: "Eat That Burger!",
+      //   description: "Cloud database for tracking burgers eaten",
+      //   github: "https://github.com/outoftune266/13-Eat-That-Burger",
+      //   href: "https://evening-lowlands-70965.herokuapp.com/",
+      //   preview: "/assets/images/eatthatburger.png",
+      // },
     ],
   };
 
